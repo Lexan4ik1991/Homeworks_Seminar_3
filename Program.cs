f71e25d5da7b315d01415ea52dee09ea258b3d2b
Lexan4ik <lexan4ik.pavloff@yandex.ru>
2022-08-31 16:01:19 +0400

    Решение задачи №2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber < secondNumber) 
+{
+    Console.WriteLine("max = secondNumber");
+}
+else
+{
+    Console.WriteLine("max = firstNumber");
+}
