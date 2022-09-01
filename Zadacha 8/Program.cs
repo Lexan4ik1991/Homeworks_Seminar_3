e961b2a806a52d15447ce45cdcd7ed04320ad531
Lexan4ik <lexan4ik.pavloff@yandex.ru>
2022-09-01 21:38:10 +0400

    Задача№8

---
+++ Zadacha 8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write number");
+int m = int.Parse(Console.ReadLine());
+for (int i = 1; i <= m; i++)
+    {
+          if (i % 2 == 0) 
+          {
+            Console.Write(i + " ");
+          }
+     }
+Console.WriteLine();
+    
