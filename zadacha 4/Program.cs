c1b5848f8131759fcec86eef45ccd29206c767db
lexan4ik <lexan4ik.pavloff@yandex.ru>
2022-09-05 12:02:39 +0300

    deleted

--- zadacha 4/Program.cs
+++ zadacha 4/Program.cs
@@ -1,12 +1,1 @@
-﻿Console.WriteLine("Enter a");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter b");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter c");
-int c = Convert.ToInt32(Console.ReadLine());
-
-int max = a;
-if (b > max)  max = b;
-if (c > b) max = c;
-
-Console.WriteLine(max);
+﻿
