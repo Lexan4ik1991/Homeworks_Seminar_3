02fa901049acd5de05a0fa5240650ff919f74448
lexan4ik <lexan4ik.pavloff@yandex.ru>
2022-09-05 12:01:05 +0300

    add

--- zadacha 4/Program.cs
+++ zadacha 4/Program.cs
@@ -1,12 +1,12 @@
-﻿Console.WriteLine("Enter a");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter b");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter c");
-int c = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Enter numberA");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter numberB");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter numberC");
+int numberC = Convert.ToInt32(Console.ReadLine());
 
-int max = a;
-if (b > max)  max = b;
-if (c > b) max = c;
+int max = numberA;
+if (numberB > max)  max = numberB;
+if (numberC > numberB) max = numberC;
 
 Console.WriteLine(max);
